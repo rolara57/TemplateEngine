66fb97588b8627d7e982f5b83f4e2bfdcb20026b
rolara57 <roger@qm46.com>
2020-09-27 20:11:43 -0500

    added to files

--- app.js
+++ app.js
@@ -9,6 +9,69 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+function appMenu(){
+    function userPrompts() {
+        return inquirer.prompt([
+            {
+                type: "list",
+                name: "employeeType",
+                message: "Employee Type:",
+                choice: ["Manager", "Engineer", "Intern"]
+            },
+            {
+                type: "input",
+                name: "name",
+                message: "What is your name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your id number?"
+            },    
+            {
+                type: "input",
+                name: "email",
+                message: "What is your email address?"
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "What is your Github username?",
+                when: function(answer) {
+                    const value = answer.employeeType == "Engineer" ? true : false;
+                    return value;
+                }
+            },
+          {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your office number?",
+            when: function(answer) {
+                const value = answer.employeeType == "Manager" ? true : false;
+                return value;
+          }  
+        },
+        {
+            type: "input",
+                name: "school",
+                message: "Which school do you attend?",
+                when: function(answer) {
+                    const value = answer.employeeType == "Intern" ? true : false;
+                    return value;
+        }
+    }
+        ]);
+    }
+    function buildTeam() {
+        if (!fs.existsSync(OUTPUT_DIR)) {
+           fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.mkdirSync(outputPath, render(answer), "utf-8");
+    }
+        userPrompts();
+    }
+    appMenu();
+
 
 
 // Write code to use inquirer to gather information about the development team members,
