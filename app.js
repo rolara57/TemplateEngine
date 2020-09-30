a76ff29fd6e4b03230341b7bd14eca3aa607e620
rolara57 <roger@qm46.com>
2020-09-30 01:07:56 -0500

    working on bugs

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+app.js
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
 const path = require("path");
 const fs = require("fs");
 
